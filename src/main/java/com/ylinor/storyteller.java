869b262854e06ac7f084c3569f480c5e5268181f
BuildTools <unconfigured@null.spigotmc.org>
2017-12-09 15:38:57 +0100

    Initialization

---
+++ src/main/java/com/ylinor/storyteller.java
@@ -0,0 +1,15 @@
+package com.ylinor;
+
+
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameInitializationEvent;
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id = "storyteller", name = "Storyteller", version = "0.0.1")
+public class storyteller {
+
+    @Listener
+    public void onServerStart(GameInitializationEvent event) {
+
+    }
+}
